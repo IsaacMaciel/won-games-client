e084a4ef1fa37aa7b54f0af8935a9fdb9a8839dc
Isaac Araujo <isaac_maciel@hotmail.com>
2021-01-27 11:43:54 -0300

    finalizacao da pagina sign up

---
+++ src/pages/sign-up.tsx
@@ -0,0 +1,9 @@
+import Auth from 'templates/Auth'
+import FormSignUp from '../components/FormSignUp'
+export default function SignIn() {
+  return (
+    <Auth title="Sign Up">
+      <FormSignUp />
+    </Auth>
+  )
+}
