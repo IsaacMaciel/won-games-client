2a624039cb5113b19229cd013007c878310df9f8
Isaac Araujo <isaac_maciel@hotmail.com>
2021-02-25 14:29:30 -0300

    add cartPage

---
+++ src/templates/Cart/stories.tsx
@@ -0,0 +1,9 @@
+import { Story, Meta } from '@storybook/react/types-6-0'
+import Cart from '.'
+
+export default {
+  title: 'Cart',
+  component: Cart
+} as Meta
+
+export const Default: Story = () => <Cart />
